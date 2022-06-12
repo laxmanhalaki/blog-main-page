adbd7392bd231983cdc58f2830ab77b336f699ac
laxmanhalaki <107349832+laxmanhalaki@users.noreply.github.com>
2022-06-12 17:05:07 +0530

    blog main-page
    blog main-page

---
+++ ndex.js
@@ -0,0 +1,9 @@
+
+let navbar=document.querySelector("#navbar");
+document.querySelector("#nav-close").onclick=()=>{
+    navbar.classList.toggle("active");
+}
+document.querySelector("#menu").onclick=()=>{
+    navbar.classList.toggle("active");
+}
+
